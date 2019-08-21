aabc337078e612ee52c7fb65bc0f4ee0e9246687
ahoer <andersahe@gmail.com>
2019-08-21 19:40:14 +0200

    unnecessary func

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -6,8 +6,8 @@ const Header = ({text, importance}) => {
 };
     
 const Total = ({parts}) => {
-    const total = () => parts.reduce((sum, part) => sum += part.exercises, 0);
-    return <b>Total of {total()} exercises</b>;
+    const total = parts.reduce((sum, part) => sum += part.exercises, 0);
+    return <b>Total of {total} exercises</b>;
 };
   
 const Part = ({name, exercises}) => <p>{name} {exercises}</p>;
