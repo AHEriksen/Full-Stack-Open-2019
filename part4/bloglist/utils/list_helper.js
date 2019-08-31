fbdfbc38cb2504011d17d719d33362a5b6556c61
ahoer <andersahe@gmail.com>
2019-08-31 12:50:14 +0200

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,15 @@
 
+
+
 const dummy = blogs => 1;
 
+const totalLikes = blogs => {
+  const reducer = (sum, item) => sum + item.likes;
+  return blogs.reduce(reducer, 0);
+};
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
+
