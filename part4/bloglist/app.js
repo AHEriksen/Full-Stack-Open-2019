3262dcf13288a5285cbc7144a5760a190be03cd6
ahoer <andersahe@gmail.com>
2019-09-16 17:37:02 +0200

    4.18 & 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 
 console.log(`connecting to ${config.MONGODB_URI}`);
@@ -22,6 +23,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
