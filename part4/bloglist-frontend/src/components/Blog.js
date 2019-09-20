264ecdfbbc752fc1df091f035a4e283564b00f61
ahoer <andersahe@gmail.com>
2019-09-20 13:43:28 +0200

    5.6

--- part4/bloglist-frontend/src/components/Blog.js
+++ part4/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [extraInfo, setExtraInfo] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderwidth: 1,
+    marginBottom: 5,
+    backgroundColor: 'silver',
+  };
+
+  const handleDisplay = () => {
+    setExtraInfo(!extraInfo);
+  };
+
+  const standardDisplay = () => (
+    <div onClick={handleDisplay}>{blog.title} {blog.author}</div>
+  );
+
+  const extraDisplay = () => (
+    <>
+      <div onClick={handleDisplay}>{blog.title} {blog.author}</div>
+      <a href={blog.url}>{blog.url}</a> <br/>
+      {blog.likes} <br/>
+      {`added by ${blog.user.name}`}
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {extraInfo ? extraDisplay() : standardDisplay()}
+      </div>
+    </div>);
+};
 
 export default Blog;
