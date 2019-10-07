ba0f4562da75783cc032e8f7a02bd738d35af9de
ahoer <andersahe@gmail.com>
2019-10-07 10:54:52 +0200

    6.18

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,15 +4,13 @@ import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
-import anecdoteService from './services/anecdotes';
 import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = (props) => {
+  const initAnecdotes = props.initializeAnecdotes;
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => props.initializeAnecdotes(anecdotes));
-  });
+    initAnecdotes();
+  }, [initAnecdotes]);
 
   return (
     <div>
