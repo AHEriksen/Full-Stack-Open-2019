f26d7d64f31ab0abbabe1c998c3c26fa0fc3ed3b
ahoer <andersahe@gmail.com>
2019-11-08 15:07:42 +0100

    8.4

--- part8/server/library-backend.js
+++ part8/server/library-backend.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
     hello: String!
   }
@@ -113,7 +113,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books;
+      else {
+        return books.filter((book) => book.author === args.author);
+      }
+    },
     allAuthors: (root) => authors,
     hello: () => { return "world" }
   },
