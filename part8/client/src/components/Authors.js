7edd40b4bacf31e4e1e29f7be123887f96284bfe
ahoer <andersahe@gmail.com>
2019-11-19 17:36:20 +0100

    8.12

--- part8/client/src/components/Authors.js
+++ part8/client/src/components/Authors.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-
+import Select from 'react-select';
 
 const Authors = ({ result, show, editAuthor }) => {
-  const [name, setName] = useState('');
-  const [setBornTo, setSetBornTo] = useState('');
+  const [born, setBorn] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
 
   if (!show) {
     return null;
@@ -12,59 +12,62 @@ const Authors = ({ result, show, editAuthor }) => {
   const submit = async (e) => {
     e.preventDefault();
 
-    await editAuthor({ variables: { name, setBornTo } } );
+    await editAuthor({ variables: { name: selectedOption.value, setBornTo: born } } );
 
-    setName('');
-    setSetBornTo('');
+    setSelectedOption(null);
+    setBorn('');
   };
 
   if (result.loading)
     return <div>loading...</div>;
+  else {
+    const options = result.data.allAuthors.reduce((obj, currentAuthor) => obj.concat({ 'value': currentAuthor.name, 'label': currentAuthor.name }), []);
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {result.data.allAuthors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+    return (
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                born
+              </th>
+              <th>
+                books
+              </th>
             </tr>
-          )}
-        </tbody>
-      </table>
-      <h3>Set birthyear</h3>
-      <form onSubmit={submit}>
-        <div>
-          name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
-        <div>
-          born
-          <input
-            value={setBornTo}
-            onChange={({ target }) => setSetBornTo(Number(target.value))}
-          />
-        </div>
-        <button type='submit'>update author</button>
-      </form>
-
-    </div>
-  );
+            {result.data.allAuthors.map(a =>
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <h3>Set birthyear</h3>
+        <form onSubmit={submit}>
+          <div>
+            <Select
+              value={selectedOption}
+              onChange={(selected) => setSelectedOption(selected)}
+              options={options}
+            />
+          </div>
+          <div>
+            born
+            <input
+              type='number'
+              value={born}
+              onChange={({ target }) => setBorn(Number(target.value))}
+            />
+          </div>
+          <button type='submit'>update author</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Authors;
